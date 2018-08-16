19a2adeb52c686182a6262b8ce72ccb55a6aab01
sejall <33982474+sejall@users.noreply.github.com>
2018-08-16 21:45:54 +0530

    Add files via upload

---
+++ multil evel inheritance.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)multil evel inheritance.java
+ *
+ *
+ * @author 
+ * @version 1.00 2018/2/25
+ */
+
+public class multil evel inheritance {
+        
+    /**
+     * Creates a new instance of <code>multil evel inheritance</code>.
+     */
+    public multil evel inheritance() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
